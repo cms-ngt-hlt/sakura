78580d5fde7f1580705b0c4e50624af424a58d2c
Marco Musich <marco.musich@cern.ch>
2025-07-10 17:56:03 +0200

    improve run ecal pedestals calibration script

--- Calibrations/runEcalPedestals.sh
+++ Calibrations/runEcalPedestals.sh
@@ -2,14 +2,20 @@
 
 # Set the environment
 export $SCRAM_ARCH=el8_amd64_gcc12
-cmsrel CMSSW_14_0_18_patch1
-cd CMSSW_14_0_18_patch1/src
-cmsenv
+CMSSW_VERSION="CMSSW_14_0_18_patch1"
+if [ -d "$CMSSW_VERSION" ]; then
+  echo "Directory $CMSSW_VERSION already exists. Skipping scram project."
+else
+  scram p CMSSW $CMSSW_VERSION
+fi
+
+cd $CMSSW_VERSION/src
+eval `scram runtime -sh`
 voms-proxy-init --voms cms
 
 # Get the configuration files
-wget https://github.com/cms-sw/cmssw/raw/refs/heads/CMSSW_14_0_X/Calibration/EcalCalibAlgos/test/ecalPedestalPCL_step1_cfg.py
-wget https://github.com/cms-sw/cmssw/raw/refs/heads/CMSSW_14_0_X/Calibration/EcalCalibAlgos/test/ecalPedestalPCL_step2_cfg.py
+wget -O ecalPedestalPCL_step1_cfg.py https://github.com/cms-sw/cmssw/raw/refs/heads/CMSSW_14_0_X/Calibration/EcalCalibAlgos/test/ecalPedestalPCL_step1_cfg.py
+wget -O ecalPedestalPCL_step2_cfg.py https://github.com/cms-sw/cmssw/raw/refs/heads/CMSSW_14_0_X/Calibration/EcalCalibAlgos/test/ecalPedestalPCL_step2_cfg.py
 
 # The streamer files are available in here 
 # `eos ls -lh /store/t0streamer/Data/Calibration/000/386/925`
@@ -41,6 +47,14 @@ EOF
 # Not go crazy
 cat <<EOF >> ecalPedestalPCL_step1_cfg.py
 process.MessageLogger.cerr.FwkReport.reportEvery = 500
+
+process.options.numberOfStreams = 8
+process.options.numberOfThreads = 8
+
+process.load('HLTrigger.Timer.FastTimerService_cfi')
+
+process.FastTimerService.writeJSONSummary = True
+process.FastTimerService.jsonFileName = "timing_ecalped_s1.json"
 EOF
 
 # This should work to produce OUT_step1.root
@@ -50,6 +64,10 @@ cmsRun ecalPedestalPCL_step1_cfg.py
 # Now fix ecalPedestalPCL_step2_cfg.py
 cat <<EOF >> ecalPedestalPCL_step2_cfg.py
 process.GlobalTag = GlobalTag(process.GlobalTag, '140X_dataRun3_Express_v3', '')
+process.load('HLTrigger.Timer.FastTimerService_cfi')
+
+process.FastTimerService.writeJSONSummary = True
+process.FastTimerService.jsonFileName = "timing_ecalped_s2.json"
 EOF
 
 # And this will produce MyPedestals.db
