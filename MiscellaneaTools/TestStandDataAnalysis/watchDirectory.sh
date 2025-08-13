60fd8822c3c5875d9039fa3aeefeb74efa32d17f
Mateusz Zarucki <mateusz.zarucki@cern.ch>
2025-08-13 19:27:00 +0200

    Update file-discovery script to only monitor new runs and improvements

--- MiscellaneaTools/TestStandDataAnalysis/watchDirectory.sh
+++ MiscellaneaTools/TestStandDataAnalysis/watchDirectory.sh
@@ -22,9 +22,8 @@ is_file_stable() {
 }
 
 NEWEST_DIR=""
-FILES=""
+seenruns=($(ls -d "$WATCH_DIR"/run*/stream"$STREAM"/data 2>/dev/null))
 seenfiles=()
-seenruns=()
 
 while true; do
   NEW_DIR=$(ls -td "$WATCH_DIR"/run*/stream"$STREAM"/data 2>/dev/null | head -n1)
@@ -34,43 +33,39 @@ while true; do
     NEWEST_DIR="$NEW_DIR"
     seenruns+=("$NEWEST_DIR")
     seenfiles=()
-    FILES=""
   fi
 
   if [[ -n "$NEWEST_DIR" ]]; then
     NEWFILES=$(ls -t "$NEWEST_DIR"/*.dat 2>/dev/null)
+    for f in $NEWFILES; do
+      fname=$(basename "$f")
+      if [[ ! " ${seenfiles[@]} " =~ (^|[[:space:]])"$fname"($|[[:space:]]) && "$fname" == *.dat ]]; then
 
-    if [ "$NEWFILES" != "$FILES" ]; then
-      for f in $NEWFILES; do
-        fname=$(basename "$f")
-        if [[ ! " ${seenfiles[*]} " =~ " $fname " && "$fname" == *.dat ]]; then
+        until is_file_stable "$f"; do
+          sleep 1
+        done
 
-          until is_file_stable "$f"; do
-            sleep 1
-          done
+        LOGFILE="$LOG_DIR/convertStreamerToFRD_${fname%.dat}.log"
 
-          echo "New streamer file detected: $fname" | eval $TIMESTAMP
-          seenfiles+=("$fname")
+        echo "New streamer file detected: $fname" | eval $TIMESTAMP >> "$LOGFILE"
+        seenfiles+=("$fname")
 
-	  RUN_NUMBER_DIR=$(basename "$(dirname "$(dirname "$NEWEST_DIR")")")
-          RUN_NUMBER=${RUN_NUMBER_DIR#run}
+        RUN_NUMBER_DIR=$(basename "$(dirname "$(dirname "$NEWEST_DIR")")")
+        RUN_NUMBER=${RUN_NUMBER_DIR#run}
 
-          LOGFILE="$LOG_DIR/convertStreamerToFRD_${fname%.dat}.log"
-          CMD_FULL="$CMD inputFiles=$f runNumber=$RUN_NUMBER"
+        CMD_FULL="$CMD inputFiles=$f runNumber=$RUN_NUMBER"
 
-          (
-            echo "Running command: $CMD_FULL" | eval $TIMESTAMP >> "$LOGFILE" &&
-            $CMD_FULL >& "$LOGFILE" && 
-            echo "Conversion completed. Deleting input streamer file $fname." | eval "$TIMESTAMP" >> "$LOGFILE" &&
-            ./addMissingEoLS.sh $RUN_NUMBER &&
-            # addMissingEoLS.sh only required for test runs not starting from LS = 1
-            sleep 5 &&
-            rm "$f"
-          ) &
-        fi
-      done
-      FILES="$NEWFILES"
+        (
+          echo "Running command: $CMD_FULL" | eval $TIMESTAMP >> "$LOGFILE" &&
+          $CMD_FULL >& "$LOGFILE" && 
+          echo "Conversion completed. Deleting input streamer file $fname." | eval "$TIMESTAMP" >> "$LOGFILE" &&
+          ./addMissingEoLS.sh $RUN_NUMBER &&
+          # addMissingEoLS.sh only required for test runs not starting from LS = 1
+          sleep 5 &&
+          rm "$f"
+        ) &
+      fi
+    done
     fi
-  fi
   sleep 2
 done
