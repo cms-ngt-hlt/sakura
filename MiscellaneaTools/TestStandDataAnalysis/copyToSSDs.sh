eac477fdb14e7ed43789efa69adfcf4b79e3451d
Mateusz Zarucki <mateusz.zarucki@cern.ch>
2025-09-03 15:11:29 +0200

    Update copy script to copy same streamer file over to SSDs

--- MiscellaneaTools/TestStandDataAnalysis/copyToSSDs.sh
+++ MiscellaneaTools/TestStandDataAnalysis/copyToSSDs.sh
@@ -1,20 +1,28 @@
 #!/bin/bash
 
-SOURCE_DIR="/fff/ssdcache/sm-c2b13-21-01_ssdcache/run392642"
-DEST_DIR="/fff/ssdcache/sm-c2b13-21-01_ssdcache/mergeMacro/run392642/streamLocalTestDataRaw/data"
+RUN_NUMBER=396309
+MAX_LS=${1:-10}
+SLEEP_TIME=23
+SOURCE_DIR="/fff/ssdcache/sm-c2b13-21-01_ssdcache/run${RUN_NUMBER}"
+SOURCE_FILE="run${RUN_NUMBER}_ls0205_streamLocalTestDataRaw_StorageManager.dat"
+DEST_BASE="/fff/ssdcache/sm-c2b13-21-01_ssdcache/mergeMacro"
+
+DEST_RUN=$((1000 + RUN_NUMBER))
+DEST_DIR="${DEST_BASE}/run${DEST_RUN}/streamLocalTestDataRaw/data"
+
 TIMESTAMP="awk '{print strftime(\"%Y-%m-%d %H:%M:%S\"), \$0}'"
 
 sudo mkdir -p "$DEST_DIR"
 
-#while true; do
-  #echo "Clearing cache... " | eval $TIMESTAMP
-  #sync
-  #echo 3 | sudo tee /proc/sys/vm/drop_caches > /dev/null 
-
-  for file in "$SOURCE_DIR"/*; do
-    [ -f "$file" ] || continue
-    echo "Copying: $(basename "$file")" | eval $TIMESTAMP
-    sudo cp "$file" "$DEST_DIR"
-    sleep 23
-  done
-#done
+for ((LS=1; LS<=MAX_LS; LS++)); do
+  OUTFILE=$(printf "run%d_ls%04d_streamLocalTestDataRaw_StorageManager.dat" "$DEST_RUN" "$LS")
+  DEST_PATH="$DEST_DIR/$OUTFILE"
+  
+  echo "Copying to: $DEST_PATH" | eval $TIMESTAMP
+  
+  sudo cp "$SOURCE_DIR/$SOURCE_FILE" "$DEST_PATH"
+  
+  if [ "$LS" -lt "$MAX_LS" ]; then
+      sleep $SLEEP_TIME
+  fi
+done
