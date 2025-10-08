283ba344fd74d1fecf5d368cfa3181380304da13
Jessica Prendi <jessica.prendi@cern.ch>
2025-10-08 16:30:28 +0200

    removing redundancies in the EcalPed plotting scripts

--- Calibrations/EcalPedestals/createTwoTagRatioAndDiffEcalPedPlots.sh
+++ Calibrations/EcalPedestals/createTwoTagRatioAndDiffEcalPedPlots.sh
@@ -1,37 +1,37 @@
 #!/bin/bash
 set -euo pipefail
 
+# reference (highest-statistics) tag
+ref_tag="EcalPedestals_NGT_2023485_test"
+
+# other tags to compare against the reference
 tags=(
-    "EcalPedestals_NGT_2023485_test"
     "EcalPedestals_NGT_1000000_test"
     "EcalPedestals_NGT_500000_test"
     "EcalPedestals_NGT_100000_test"
     "EcalPedestals_NGT_75000_test"
 )
 
-for ((i=0; i<${#tags[@]}; i++)); do
-    for ((j=i+1; j<${#tags[@]}; j++)); do
-        tag1="${tags[i]}"
-        tag2="${tags[j]}"
+for tag in "${tags[@]}"; do
+    echo "Processing comparison: ${ref_tag} vs ${tag}"
 
-        echo "Processing pair: $tag1 vs $tag2"
+    for plot in plot_EcalPedestalsDiffTwoTags plot_EcalPedestalsRatioTwoTags; do
+        echo "  → Generating $plot"
 
-        for plot in plot_EcalPedestalsDiffTwoTags plot_EcalPedestalsRatioTwoTags; do
-            getPayloadData.py \
-                --plugin pluginEcalPedestals_PayloadInspector \
-                --plot "$plot" \
-                --tag "$tag1" \
-                --tagtwo "$tag2" \
-                --input_params "{}" \
-                --time_type Run \
-                --iovs '{"start_iov": "400000", "end_iov": "400000"}' \
-                --iovstwo '{"start_iov": "400000", "end_iov": "400000"}' \
-                --db Prep --test >& out.json
+        getPayloadData.py \
+            --plugin pluginEcalPedestals_PayloadInspector \
+            --plot "$plot" \
+            --tag "$ref_tag" \
+            --tagtwo "$tag" \
+            --input_params "{}" \
+            --time_type Run \
+            --iovs '{"start_iov": "400000", "end_iov": "400000"}' \
+            --iovstwo '{"start_iov": "400000", "end_iov": "400000"}' \
+            --db Prep --test >& out.json
 
-            filename=$(tail -n +2 out.json | jq -r '.file')
-            filename=$(head -n 1 out.json | awk '{print $6}')
-            mv "$filename" "${plot}_${tag1}_vs_${tag2}.png"
-        done
+        filename=$(tail -n +2 out.json | jq -r '.file')
+
+        mv "$filename" "${plot}_${ref_tag}_vs_${tag}.png"
     done
 done
 
